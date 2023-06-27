979c8c18c248be33000088a4b72af96094a4b1ee
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-06-26 21:47:50 -0400

    first commit

---
+++ controllers/logsController.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const logs = express.Router();
+const logsArray = require("../models/logs.js");
+
+logs.get("/", (req, res) => {
+    res.json(logsArray);
+  });
+  
+
+  module.exports = logs;
