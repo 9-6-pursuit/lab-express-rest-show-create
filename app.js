22def44f46d2000ed6c0ab85994c75173c1694a2
Lakisha J <lakishajohnson@pursuit.org>
2023-06-27 22:25:19 -0400

    Tests passing, working on BONUSES

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+//DEPENDENCIES
 const express = require("express");
-
-const app = express();
-
-
 const captainsLogController = require("./controllers/captainsController");
 
+//CONFIGURATION
+const app = express();
+
+//MIDDLEWARE
 app.use(express.json());
 
+//ROUTES
 app.use("/logs", captainsLogController);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Captain's Log");
 });
 
+app.get("/logs", (req, res) => {
+  res.json(logEntries);
+});
+
 app.get("*", (req, res) => {
   res.status(404).json({ error: "Page not found" });
 });
+// app.get("*", (req, res) => {
+//   res.status(401).send(`<img src="https://http.cat/400">`);
+// });
 
 module.exports = app;
--- controllers/captainsController.js
+++ controllers/captainsController.js
@@ -1,11 +1,48 @@
+//DEPENDENCIES
 const express = require("express");
-const logs = express.Router();
 const logsArray = require("../models/captainsLog");
 // console.log(logsArray)
+
+//CONFIGURATION
+const logs = express.Router();
 
 // INDEX
 logs.get("/", (req, res) => {
   res.json(logsArray);
 });
+// logs.get("/", (req, res) => {
+//     res.json(logsArray);
 
-module.exports = logs
+//   if (order === "asc") {
+//       const sortedLogs = logEntries
+//         .slice()
+//         .sort((a, b) => a.captainName.localeCompare(b.captainName));
+//       res.json(sortedLogs);
+//     } else {
+//       // Handle other cases or fallback behavior
+//       res.json(logEntries);
+//     }
+//   });
+
+//SHOW
+logs.get("/:arrayIndex", (req, res) => {
+  if (logsArray[req.params.arrayIndex]) {
+    res.json(logsArray[req.params.arrayIndex]);
+  } else {
+    res.redirect("*");
+  }
+});
+
+// CREATE
+logs.post("/", (req, res) => {
+  logsArray.push(req.body);
+  res.json(logsArray[bookmarksArray.length - 1]);
+});
+
+// DELETE
+logs.delete("/:indexArray", (req, res) => {
+  const deletedLog = logsArray.splice(req.params.indexArray, 1);
+  res.status(200).json(deletedLog);
+});
+
+module.exports = logs;
