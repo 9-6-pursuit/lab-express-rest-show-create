01632503660bdc959a3f33a94397870ba9704a98
Judy <judychue@pursuit.org>
2023-06-27 22:31:05 -0400

    all passed

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,10 +1,42 @@
 const express = require("express");
-const logs = express.Router();
+// const logs = express.Router();
+const logs = express();
 const logsArray = require("../models/log.js");
 
-// LOGS
+logs.use(express.json());
+
+// INDEX
 logs.get("/", (req, res) => {
-  res.json(logsArray);
+    const foo = JSON.parse(JSON.stringify(logsArray));
+  res.status(200).json(foo);
 });
+
+// CREATE
+logs.post("/", (req, res) => {
+    logsArray.push(req.body);
+        res.json(logsArray[ogsArray.length - 1]);
+    });
+
+// SHOW
+logs.get("/:arrayIndex", (req, res) => {
+    if (logsArray[req.params.arrayIndex]) {
+        res.json(logsArray[req.params.arrayIndex]);
+    } else {
+        res.status(404).redirect("/404");
+    }
+    });
+
+// DELETE
+logs.delete("/:indexArray", (req, res) => {
+    const deletedlog = logsArray.splice(req.params.indexArray, 1);
+    res.status(200).json(deletedlog);
+  });
+  
+// UPDATE
+  logs.put("/:arrayIndex", (req, res) => {
+    logsArray[req.params.arrayIndex] = req.body;
+    res.status(200).json(logsArray[req.params.arrayIndex]);
+  });
+  
 
 module.exports = logs;
