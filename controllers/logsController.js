cad4721e2b6c8b0664215ce1eca16b3f18f43a58
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-07-02 18:22:34 -0400

    Finished part 3

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -21,4 +21,25 @@ logs.post("/", (req, res) => {
   logsArray.push(req.body);
   res.json(logsArray[logsArray.length - 1]);
 });
+
+// DELETE
+logs.delete("/:indexArray", (req, res) => {
+  if (logsArray[req.params.arrayIndex]) {
+    const deletedBookMark = logsArray.splice(req.params.indexArray, 1);
+    res.status(200).json(deletedBookMark);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
+// UPDATE
+logs.put("/:arrayIndex", (req, res) => {
+  if (logsArray[req.params.arrayIndex]) {
+    logsArray[req.params.arrayIndex] = req.body;
+    res.status(200).json(logsArray[req.params.arrayIndex]);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
 module.exports = logs;
