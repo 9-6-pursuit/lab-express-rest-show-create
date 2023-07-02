4e36bbd72a0f987072827aff7c4f5765268235ce
Monir Hossen <monirhossen@pursuit.org>
2023-07-02 12:29:47 -0400

    completed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 //Routes
 app.get("/", (req, res)=>{
-    res.send("Welcome to captain's log")
+    res.send("Welcome to captain's log!")
 })
 
 
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -3,9 +3,36 @@ const logs = express.Router()
 const logsArray= require("../models/log.js")
 
 
-//index
+//Index
 logs.get("/", (req, res)=>{
     res.json(logsArray)
 })
 
-module.exports=logs
+//Show
+logs.get("/:arrayIndex", (req, res)=>{
+    if(logsArray[req.params.arrayIndex]){
+        res.json(logsArray[req.params.arrayIndex])
+    } else {
+        res.status(404).redirect("/404")
+    }
+})
+
+//Create
+logs.post("/", (req, res)=>{
+    logsArray.push(req.body)
+    res.json(logsArray[logsArray.length-1])
+})
+
+//Delete
+logs.delete("/:arrayIndex", (req, res)=>{
+    if(logsArray[req.params.arrayIndex]){
+        const deletedLog = logsArray.splice(req.params.arrayIndex, 1)
+        res.json(deletedLog)
+    } else {
+        res.status(404).redirect("/404")
+    }
+})
+
+
+
+module.exports=logs;
