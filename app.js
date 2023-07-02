52b54491faf968a524e8575c476a24a928bff942
Ari-So-Irie <arius1500@gmail.com>
2023-07-01 20:36:33 -0400

    Lab complete comments added

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-// DEPENDENCIES
+// DEPENDENCIES 
+//Importing the express module
 const express = require("express");
 
 // CONFIGURATION
+//Creating a express app
 const app = express();
 
 // MIDDLEWARE
-app.use(express.json()); // Parse incoming JSON
-
+// Parse incoming JSON
+app.use(express.json()); 
+// Importing the logs controller 
 const logsController = require("./controllers/logsController.js");
 app.use("/logs", logsController);
 // ROUTES
+//home route
 app.get("/", (req, res) => {
   res.send("welcome to the captain's log");
 });
 
 // 404 PAGE
+// "*" is an error catch all command
 app.get("*", (req, res) => {
-    res.status(404).json({ error: "Page not found" });
+  // If the requested route does not exist, return a JSON response with an error message and set the status to 404 (Not Found)
+    res.status(404).json({ error: "Not found" });
   });
 
-// EXPORT
+//Exporting the express app
 module.exports = app;
